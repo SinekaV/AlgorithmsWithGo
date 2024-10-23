149cae08c0cbb5b89276739c6ebd1f160a4c0591
Sineka V <106376461+SinekaV@users.noreply.github.com>
2024-10-23 14:56:29 +0530

    Update sumOfNumInList.go

--- sumOfNumInList.go
+++ sumOfNumInList.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
- 
+
+func recursiveSum(list []int) int{
+   if(len(list) == 0){
+    return 0}
+    return list[0]+ recursiveSum(list[1:])
+
+}
+
 func sum(list []int) int {
     total := 0
     for _, val := range list {
@@ -13,5 +20,7 @@ func sum(list []int) int {
 func main() {
     list := []int{1, 1, 1, 1, 1, 1}
     totalSum := sum(list)
+    totalSumByRecursion := recursiveSum(list)
     fmt.Printf("Sum of the list of numbers: %d\n", totalSum)
+    fmt.Printf("Sum of the list of numbers: %d\n", totalSumByRecursion)
 }
